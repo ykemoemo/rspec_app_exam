062c56b6ed418496195e02fc6b0c865c5d834a61
Yamashita <yk.emoemo@gmail.com>
2021-06-09 19:40:45 +0900

    Fix: Factoryにassociationを設定することでproject_idの指定を省略

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Task', type: :system do
   let(:project) { create(:project) }
-  let(:task) { create(:task, project_id: project.id) }
-  let(:task_done) { create(:task, :done, project_id: project.id) }
+  let(:task) { create(:task) }
+  let(:task_done) { create(:task, :done) }
 
   describe 'Task一覧' do
     context '正常系' do
@@ -46,11 +46,11 @@ RSpec.describe 'Task', type: :system do
     context '正常系' do
       it 'Taskが表示されること' do
         # TODO: ローカル変数ではなく let を使用してください
-        visit project_task_path(project, task)
+        visit project_task_path(task, task.id)
         expect(page).to have_content(task.title)
         expect(page).to have_content(task.status)
         expect(page).to have_content(task.deadline.strftime('%Y-%m-%d %H:%M'))
-        expect(current_path).to eq project_task_path(project, task)
+        expect(current_path).to eq project_task_path(task, task.id)
       end
     end
   end
@@ -59,32 +59,32 @@ RSpec.describe 'Task', type: :system do
     context '正常系' do
       it 'Taskを編集した場合、一覧画面で編集後の内容が表示されること' do
         # FIXME: テストが失敗するので修正してください
-        visit edit_project_task_path(project, task)
+        visit edit_project_task_path(task, task.id)
         fill_in 'Deadline', with: Time.current
         click_button 'Update Task'
         click_link 'Back'
         expect(find('.task_list')).to have_content(short_time(Time.current))
-        expect(current_path).to eq project_tasks_path(project)
+        expect(current_path).to eq project_tasks_path(task)
       end
 
       it 'ステータスを完了にした場合、Taskの完了日に今日の日付が登録されること' do
         # TODO: ローカル変数ではなく let を使用してください
-        visit edit_project_task_path(project, task)
+        visit edit_project_task_path(task, task.id)
         select 'done', from: 'Status'
         click_button 'Update Task'
         expect(page).to have_content('done')
         expect(page).to have_content(Time.current.strftime('%Y-%m-%d'))
-        expect(current_path).to eq project_task_path(project, task)
+        expect(current_path).to eq project_task_path(task, task.id)
       end
 
       it '既にステータスが完了のタスクのステータスを変更した場合、Taskの完了日が更新されないこと' do
         # TODO: FactoryBotのtraitを利用してください
-        visit edit_project_task_path(project, task_done)
+        visit edit_project_task_path(task_done, task_done.id)
         select 'todo', from: 'Status'
         click_button 'Update Task'
         expect(page).to have_content('todo')
         expect(page).not_to have_content(Time.current.strftime('%Y-%m-%d'))
-        expect(current_path).to eq project_task_path(project, task_done)
+        expect(current_path).to eq project_task_path(task_done, task_done.id)
       end
     end
   end
@@ -93,12 +93,12 @@ RSpec.describe 'Task', type: :system do
     context '正常系' do
       # FIXME: テストが失敗するので修正してください
       it 'Taskが削除されること' do
-        visit project_tasks_path(project, task)
+        visit project_tasks_path(task)
         click_link 'Destroy'
         page.driver.browser.switch_to.alert.accept
         expect(find('.task_list')).not_to have_content task.title
         expect(Task.count).to eq 0
-        expect(current_path).to eq project_tasks_path(project)
+        expect(current_path).to eq project_tasks_path(task)
       end
     end
   end
