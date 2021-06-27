f4fd4919290c93a2537d82c56835f144bf8b99ca
ykemoemo <ykemoemo@gmail.com>
2021-06-27 12:37:47 +0900

    factory :taskの内容を変更

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     association :project, factory: :project
     title { 'Task' }
-    status { rand(3) }
+    status { rand(2) }
     from = Date.parse("2019/08/01")
     to   = Date.parse("2019/12/31")
     deadline { Random.rand(from..to) }
